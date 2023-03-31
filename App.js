d3c1cd9f9f311d4b17e36408d8ddfb4dd99fd377
sambba1 <samuli-lehtinen@hotmail.fi>
2023-03-31 11:14:05 +0300

    forecast

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
+import { React, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
 import  Weather  from './components/Weather';
 
 export default function App() {
+
+
+  const data = [
+    { id: '1', text: 'Page 1' },
+    { id: '2', text: 'Page 2' },
+  ];
+
+  const screenWidth = Dimensions.get('window').width;
+
+  const renderItem = ({ item }) => (
+    <View style={{ width: screenWidth, height: '100%', backgroundColor: '#28587a',}}>
+      <Weather/>
+    </View>
+  );
+
   return (
-    <View style={styles.container}>
-      <Weather />
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={screenWidth+50}
+          decelerationRate="fast"
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          contentContainerStyle={{ justifyContent: 'center' }}
+        />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#99EDF9',
+    backgroundColor: '#28587a',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/Weather.js
+++ components/Weather.js
@@ -1,61 +1,111 @@
 import { React, useState } from 'react';
-import { View, Text, Image,StyleSheet } from 'react-native';
+import { View, Text, Image,StyleSheet, ScrollView } from 'react-native';
 import * as Location from 'expo-location';
 import { useEffect } from 'react';
 import { APIKEY } from '../api.js';
 
 export default function Weather() {
 
-    const [isReady, setReady] = useState(false);
-    const [location, setLocation] = useState(null);
-    const [city, setCity] = useState('');
-    const [weather, setWeather] = useState('');
-    const [temp, setTemp] = useState('');
-    const [weatherIconUrl, setWeatherIconUrl] = useState('');
-
+    const [isReady, setReady] = useState(true);
+    const [weatherData, setWeatherData] = useState('');
+    const [forecastData, setForecastData] = useState('');
 
     
-    useEffect(() => {
+    const getWeatherData = async () => {
         setReady(false);
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                Alert.alert('No permission to get location')
-                return;
-            }
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-            fetchData();
-        })();
-    }, []);
 
-    const fetchData = async () => {
-            
-        
-        try {
-          const response = await fetch('https://api.openweathermap.org/data/2.5/weather?'+ 'lat=' + location.coords.latitude + '&lon=' + location.coords.longitude + '&units=metric&appid=' + APIKEY);
-          const data = await response.json();
-          setTemp(data.main.temp);
-          setWeather(data.weather[0].main);
-          setCity(data.name);
-          setWeatherIconUrl("http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-        } catch(error){
-          console.error(error);
-        } finally {
-          setReady(true);
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            Alert.alert('No permission to get location')
+            return;
         }
+        let location = await Location.getCurrentPositionAsync({});
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?'+ 'lat=' + location.coords.latitude + '&lon=' + location.coords.longitude + '&units=metric&appid=' + APIKEY + "&lang=fi");
+        const data = await response.json();
+        setWeatherData(data);
+        const response2 = await fetch('https://api.openweathermap.org/data/2.5/forecast?'+ 'lat=' + location.coords.latitude + '&lon=' + location.coords.longitude + '&units=metric&appid=' + APIKEY);
+        const data2 = await response2.json();
+        
+        let weatherDataWithKeys = data2.list.map((data, index) => {
+            let key = index;
+            return { key: key, ...data };
+        });
+        setForecastData(weatherDataWithKeys);
+        setReady(true);
+        
     };
+   
+    const forecast = () =>{
+        forecastData.forEach((data, index) => {
+            const key = `{index}`;
+        });
+        console.log(forecastData[0])
+        return (
+            <View style={{backgroundColor: 'rgba(120, 120, 120, 0.3)',
+            borderRadius: 10,
+            overflow: 'hidden',
+            marginVertical: 20,
+            marginHorizontal: 10,
+            height:120,
+            width: "90%"}}>
+            <Text style={{marginLeft: 20, fontSize: 15}}>Kolmen tunnin ennuste</Text>
+            <ScrollView 
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            >
+                <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center',}}>
+                    
+                    {forecastData.slice(1, 20).map(item => 
+                    <View key={item.key} style={{ marginLeft: 20, alignItems: 'center'}}>
+                        <Text style={{fontSize: 20}}>{new Date(item.dt_txt).getHours()}</Text>
+                        {DrawIcon(item.weather[0].icon, 50)}
+                        <Text style={{fontSize: 20}}>{Math.round(item.main.temp) +"°"}</Text>
+                    </View>
+                    )}
+                </View>
+            </ScrollView>
+            </View>
+
+        );
+    } 
+
+
+
+    const DrawIcon = (iconName, size) =>{
+        weatherIconUrl = "http://openweathermap.org/img/w/" + iconName + ".png"
+        return (
+            <Image
+                style={{ width: size, height: size }}
+                source={{ uri: weatherIconUrl }}
+            />
+        );
+        
+    }
+
+
 
     if(isReady){
+        if (weatherData.length == 0){
+            getWeatherData();
+            return;
+        }
         return (
-            <View style={styles.container}>
-                <Text style={{fontSize: 50}}>{ city }</Text>
-                <Text style={{fontSize: 20}}>{ temp }</Text>
-                <Text style={{fontSize: 20}}>{ weather }</Text>
-                <Image
-                    style={{ width: 100, height: 100 }}
-                    source={{ uri: weatherIconUrl }}
-                />
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text style={{fontSize: 50}}>{ weatherData.name }</Text>
+                    <Text style={{fontSize: 50}}>{ Math.round(weatherData.main.temp )+"°" }</Text>
+                    <Text style={{fontSize: 20}}>{ weatherData.weather[0].description }</Text>
+                    {DrawIcon(weatherData.weather[0].icon, 100)}
+                </View>
+                <View style={{ flex: 1, alignItems: 'center', justifyContent:'center'}}>
+                    {forecast()}
+                </View>
+                <View style={{
+                    flex: 2,
+
+                    }}>
+
+                </View>
             </View>
         );
     }else{
@@ -69,12 +119,4 @@ export default function Weather() {
 
 };
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#99EDF9',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
 
